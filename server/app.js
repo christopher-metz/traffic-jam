0773e781470afcaf43e41ee8f32f3ab4be4fc0a7
Christopher Metz <cmetz7@gmail.com>
2017-02-22 14:39:23 -0800

    Deployed to heroku

--- server/app.js
+++ server/app.js
@@ -3,11 +3,18 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const morgan = require('morgan');
+const path = require('path');
 const app = express();
 
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 
 app.use('/api', require('./routes/api'))
+
+app.use(express.static(path.resolve(__dirname, '..', 'build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
+});
 
 module.exports = app;
